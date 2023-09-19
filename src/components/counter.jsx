d0f9ed5e4eab827a614e015b58d40bec8b6f0724
Bikalp Timalsina <bikalp16@gmail.com>
2023-09-18 20:58:51 -0700

    impl condition where counter won't go below 0

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,26 @@ import { useState } from "react";
 import styles from "../styles/counter.module.css";
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  const counter = 0;
+  const [count, setCount] = useState(counter);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
   return (
     <div className={styles.main}>
       <div className={styles.counterBody}>
-        <button
-          className={styles.button}
-          onClick={() => setCount((currCount) => count + 1)}
-        >
+        <button className={styles.button} onClick={increaseCount}>
           increment
         </button>
         <div className={styles.countNumber}>{count}</div>
-        <button
-          className={styles.button}
-          onClick={() => setCount((currCount) => count - 1)}
-        >
+        <button className={styles.button} onClick={decreaseCount}>
           decrement
         </button>
       </div>
